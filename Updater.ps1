3e41057a7d4db8d23bfea2847eceb64d72a8d6ff
Martin Roszak <roszak@incas.de>
2021-07-30 12:19:33 +0200

    Update URLs

--- Updater.ps1
+++ Updater.ps1
@@ -1,5 +1,8 @@
 ﻿$PSScriptRoot
-$installedversion = (Get-Content  -Path '.\GPU Updater Tool.ps1')[0].Split('=')[1]
-$onlineversion = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPU%20Updater%20Tool.ps1").Content.Substring(10,3)
+$installedversion = (Get-Content  -Path '.\GPUUpdaterTool.ps1')[0].Split('=')[1]
+if ((gwmi win32_operatingsystem | % caption) -like '*Windows 10*') {
+    $onlineversion = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/657870/Cloud-GPU-Updater/feature/windows10-support/GPUUpdaterTool.ps1" -UseBasicParsing).Content.Substring(10,3)
+} else {
+ $onlineversion = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/657870/Cloud-GPU-Updater/feature/windows10-support/GPUUpdaterTool.ps1").Content.Substring(10,3)
+}
 if ($onlineversion -gt $installedversion){"Update"} Else {"No Update"}
-#https://github.com/jamesstringerparsec/Cloud-GPU-Updater/archive/dev.zip
